fd544ea96f0e2d1f2be4286fd10d757fddc71155
Sergio Andreu <sergio.andreu@cuatroochenta.com>
2024-04-30 12:56:42 +0200

    API aplicada

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,28 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-
-import 'package:cinemapedia/config/router/app_router.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:cinemapedia/config/router/app_router.dart';
 import 'package:cinemapedia/config/theme/app_theme.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-Future<void> main() async{
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  }
+}
 
+void main() async {
   await dotenv.load(fileName: '.env');
-  runApp(const MainApp());
+  HttpOverrides.global = MyHttpOverrides();
+  runApp(
+    const ProviderScope(child: MainApp()),
+  );
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
